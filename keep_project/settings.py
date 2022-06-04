895c77e79c1fa1f04d4e46cbb32a3ee53f8528a0
ChisengaSol <solmanzambia@gmail.com>
2022-06-04 16:46:26 +0200

    changed database config

--- keep_project/settings.py
+++ keep_project/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("NOTEKEEPING_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = ["*"]
 
