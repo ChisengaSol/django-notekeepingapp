2c271c6877b57540e3ff5e141c924afba643f66c
ChisengaSol <solmanzambia@gmail.com>
2022-06-04 13:15:49 +0200

    connected to postgres db

--- keep_project/settings.py
+++ keep_project/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+import environ
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-slncrk5#+w6*!$dgpne6v8*s(qvi&b1(_&2uyn5=%!pq0fan4+'
+SECRET_KEY = env("NOTEKEEPING_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -76,9 +82,13 @@ WSGI_APPLICATION = 'keep_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("NOTEKEEPING_DATABASE_NAME"),
+        "USER": env("NOTEKEEPING_DATABASE_USER"),
+        "PASSWORD": env("NOTEKEEPING_DATABASE_PASSWORD"),
+        "HOST": env("NOTEKEEPING_DATABASE_HOST"),
+        "PORT": env("NOTEKEEPING_DATABASE_PORT"),
     }
 }
 
