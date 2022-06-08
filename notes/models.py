678e6cbf962a8228f4427454f00f0a6b00706167
ChisengaSol <solmanzambia@gmail.com>
2022-06-08 20:11:32 +0200

    changed max_length for the charfield on title

--- notes/models.py
+++ notes/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Note(models.Model):
-    title=models.CharField(max_length=45,null=False)
+    title=models.CharField(max_length=1000,null=False)
     description=models.TextField(null=False)
     author=models.ForeignKey(User,on_delete=models.CASCADE)
 
